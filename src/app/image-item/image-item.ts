13b2a46459b04d7cefa65af8a65b2e47ccf12841
nereame96 <nereame96@gmail.com>
2025-11-23 18:55:50 +0100

    feat: add tailwind conf and responsive grid layout

--- src/app/image-item/image-item.ts
+++ src/app/image-item/image-item.ts
@@ -15,6 +15,14 @@ export class ImageItemComponent {
   image = input.required<Image>()
   isFeatured = input(false)
 
+  @HostBinding('class.col-span-2')
+    get colSpan() {
+      return this.isFeatured()
+    }
+  @HostBinding('class.row-span-2')
+    get rowSpan() {
+      return this.isFeatured()
+    }
 
   @HostBinding('class.featured')
 
